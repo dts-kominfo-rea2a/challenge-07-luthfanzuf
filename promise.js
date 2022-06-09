70113d1fed9e725a6ef979592f5f0c2d181c3b85
luthfanzuf <luthfanzuf@gmail.com>
2022-06-09 22:59:38 +0700

    commit

--- promise.js
+++ promise.js
@@ -1,7 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (paramString) => {
+  try{
+    let hasilTheaterIXX = await promiseTheaterIXX();
+    let hasilTheaterVGC = await promiseTheaterVGC();
+    let hasilSemuaTheater = hasilTheaterIXX.concat(hasilTheaterVGC);
+
+    let counter = 0
+
+    for(obj of hasilSemuaTheater) {
+      if(paramString === obj.hasil) {
+        counter = counter + 1
+      } else {
+        counter = counter + 0
+      }
+    }
+    return counter
+  }
+  catch (error){
+    console.log('error')
+  }
+}
+promiseOutput()
 
 module.exports = {
   promiseOutput,
